12103aa7e048061e010640c20d92f3bfcc8bbcf9
TanguyMrtn <43412089+TanguyMrtn@users.noreply.github.com>
2019-01-07 08:46:31 +0100

    Delete displayGameSVG.py

--- displayGameSVG.py
+++
@@ -1,8 +0,0 @@
-import chess
-import chess.svg
-from IPython.display import SVG,display
-
-#This program allow us to display a chess game (display the board)
-
-board=chess.Board()
-display(SVG(chess.svg.board(board=board)))
